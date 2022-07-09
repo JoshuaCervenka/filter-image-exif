3d2054158f3088d6a7982c97008e2994318569ff
JoshuaCervenka <joshcervenka32@gmail.com>
2022-07-09 10:59:06 -0500

    Update sendJpeg_SF.bash

--- sendJpeg_SF.bash
+++ sendJpeg_SF.bash
@@ -8,6 +8,6 @@
 # open windows and start application
 #xterm $xterm_options -title "${id}"\
 #    -e "
-./snap_jpegStandalone 127.0.0.1 5311 2 SFground test.jpg
+./jpegStandalone 127.0.0.1 5311 2 SFground test.jpg
 
 sleep 5
